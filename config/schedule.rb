cbc671f93538a16efa67c5e84801d781fda821bc
soikawa <soikawa0108@gmail.com>
2023-09-01 17:35:07 +0900

    whenever完了

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,39 +1,40 @@
 # config/schedule.rb
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :environment, "development" # 開発環境で動作（本番環境で動かす場合は注意）
 set :output, "log/cron_log.log"
 set :runner_command, "rails runner"
 
-every 1.minute do # 1.minute
-  command "echo 'ｺﾝﾆﾁﾜ'"
-end
+#every 1.minutes do
+  #runner "SendPhotoJob.perform_now('late_evening')"
+#end
 # 0時〜3時 "未明"
 every 1.day, at: '1:30 am' do
-  runner "SendPhotoJob.perform_later('midnight')"
+  runner "SendPhotoJob.perform_now('midnight')"
 end
 
 # 3時〜6時 "明け方"
 every 1.day, at: '4:00 am' do
-  runner "SendPhotoJob.perform_later('early_morning')"
+  runner "SendPhotoJob.perform_now('early_morning')"
 end
 
 # 6時〜9時 "朝"
 every 1.day, at: '8:00 am' do
-  runner "SendPhotoJob.perform_later('morning')"
+  runner "SendPhotoJob.perform_now('morning')"
 end
 
 # 9時〜12時 "昼前"
 every 1.day, at: '10:30 am' do
-  runner "SendPhotoJob.perform_later('late_morning')"
+  runner "SendPhotoJob.perform_now('late_morning')"
 end
 
 # 12時〜15時 "昼過ぎ"
-every 1.day, at: '12:00 pm' do
-  runner "SendPhotoJob.perform_later('early_afternoon')"
+every 1.day, at: '12:45 am' do
+  runner "SendPhotoJob.perform_now('early_afternoon')"
 end
 
 # 15時〜18時 "夕方"
 every 1.day, at: '4:00 pm' do
-  runner "SendPhotoJob.perform_later('late_afternoon')"
+  runner "SendPhotoJob.perform_now('late_afternoon')"
 end
 
 # 18時〜21時 "夜のはじめ頃"
@@ -42,8 +43,6 @@ every 1.day, at: '7:00 pm' do
 end
 
 # 21時〜24時 "夜遅く"
-every 1.day, at: '10:00 pm' do # '17:50 pm' を '9:50 pm' に修正
-  runner "SendPhotoJob.perform_later('late_evening')"
+every 1.day, at: '11:00 pm' do 
+  runner "SendPhotoJob.perform_now('late_evening')"
 end
-
-env :PATH, ENV['PATH']
