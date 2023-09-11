68cee09d3bff1a35861d63566551f4ab1d30ed96
soikawa <soikawa0108@gmail.com>
2023-09-12 00:02:35 +0900

    修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,10 +6,35 @@ class Photo < ApplicationRecord
   mount_uploader :image, CoverImageUploader
   validates :image, file_size: { less_than_or_equal_to: 400.kilobytes }
   attr_accessor :tag_names
+  after_save :process_image
 
   validates :image, presence: true
   validates :album_id, presence: true
   validates :capture_date, presence: true
+
+  after_save :process_image
+  
+  def process_image
+    return unless image_path.present?
+  
+    temp_dir = Dir.mktmpdir('image_processing')
+  
+    begin
+      original_image_path = Rails.root.join('path', 'to', 'storage', image_path)
+  
+      processed_image_path = File.join(temp_dir, 'processed_image.jpg')
+        
+      image = MiniMagick::Image.open(original_image_path)
+      image.resize '800x600'
+      image.write processed_image_path
+        
+        # ここでさらなる処理を行ったり、processed_image_pathを永続ストレージに保存したりします。
+        
+    ensure
+        # 使用後に一時ディレクトリを削除
+      FileUtils.remove_entry temp_dir
+    end
+  end
 
   def save_tags(sent_tags)
     current_tags = self.tags.pluck(:tag_names) || []
