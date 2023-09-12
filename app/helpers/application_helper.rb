ec9b223ea05bb0086785d40b4d90ec30785bf78f
soikawa <soikawa0108@gmail.com>
2023-09-12 16:52:02 +0900

    OGP再修正2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
       title: 'あなたが愛する「推し」との思い出の写真をLINEbotでランダムに届けてくれるサービスです。',
       reverse: true,
       charset: 'utf-8',
-      description: 'osimemorysを使えば、「推し」との思い出が簡単に振り返れます',
+      description: 'osimemorysを使えば、「推し」との思い出が簡単に振り返れます。',
       keywords: 'osimemorys',
       canonical: request.original_url,
       separator: '|',
@@ -22,7 +22,7 @@ module ApplicationHelper
       twitter: {
         card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
         site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('3-1.png') # 配置するパスやファイル名によって変更すること
+        image: image_url('Angelina.png') # 配置するパスやファイル名によって変更すること
       }
     }
   end
