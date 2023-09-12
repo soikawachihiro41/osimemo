2c9b2f91c9e37931b5b298626391fe8a6838f5a1
soikawa <soikawa0108@gmail.com>
2023-09-12 21:23:04 +0900

    セキュリティ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :albums
   has_many :birthday_notifications
   has_one :notification_setting, class_name: 'NotificationSetting'
-  validates :name, presence: true
-  #validates :line_user_id, presence: true, uniqueness: true
+  #validates :name, presence: true
+  validates :line_user_id, presence: true, uniqueness: true
 
   enum role: { general: 0, admin: 1 }
 
