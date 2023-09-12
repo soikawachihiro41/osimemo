9fbc9f9539ecb6b31a3f651bee765d4cee13c16b
soikawa <soikawa0108@gmail.com>
2023-09-12 16:47:05 +0900

    OGP再修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'Pluspo',
-      title: '都内スポーツ施設の横断検索サービス',
+      title: 'あなたが愛する「推し」との思い出の写真をLINEbotでランダムに届けてくれるサービスです。',
       reverse: true,
       charset: 'utf-8',
-      description: 'あなたが愛する「推し」との思い出の写真をLINEbotでランダムに届けてくれるサービスです。',
+      description: 'osimemorysを使えば、「推し」との思い出が簡単に振り返れます',
       keywords: 'osimemorys',
       canonical: request.original_url,
       separator: '|',
@@ -15,14 +15,14 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('3.png'), # 配置するパスやファイル名によって変更すること
+        image: image_url('3-1.png'), # 配置するパスやファイル名によって変更すること
         local: 'ja-JP'
       },
       # Twitter用の設定を個別で設定する
       twitter: {
         card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
         site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('3.png') # 配置するパスやファイル名によって変更すること
+        image: image_url('3-1.png') # 配置するパスやファイル名によって変更すること
       }
     }
   end
