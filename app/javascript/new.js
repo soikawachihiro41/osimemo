1f4bf4fb062de983d9f7e4b17933c8d246255d6e
soikawa <soikawa0108@gmail.com>
2023-09-19 10:05:37 +0900

    ログイン修正完成

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,38 +1,34 @@
 document.addEventListener('turbo:load', () => {
   const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   const LIFF_ID = gon.user_key;
-  liff
-    .init({
-      liffId: LIFF_ID,
-      withLoginOnExternalBrowser: true
-    })
-  liff
-    .ready.then(() => {
-      if (!liff.isLoggedIn()) {
-        liff.login();
-      } else {
-        liff.getProfile().then(profile => {
-          const idToken = liff.getIDToken()
-          const name = profile.displayName
-          const body = `idToken=${idToken}&name=${name}`
-          const request = new Request('/users', {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-              'X-CSRF-Token': token
-            },
-            method: 'POST',
-            body: body
-          });
 
-          fetch(request)
-            .then(response => response.json())
-            .then(data => {
-              data_id = data
-            })
-            .then(() => {
-              window.location = '/after_login'
-            })
-        })
-      }
-    })
-})
+  liff.init({
+    liffId: LIFF_ID,
+    withLoginOnExternalBrowser: true
+  });
+
+  liff.ready.then(() => {
+    const idToken = liff.getIDToken();
+    const name = profile.displayName;
+    const body = `idToken=${idToken}&name=${name}`;
+
+    const request = new Request('/users', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'X-CSRF-Token': token
+      },
+      method: 'POST',
+      body: body
+    });
+
+    fetch(request)
+      .then(response => response.json())
+      .then(data => {
+        data_id = data;
+      })
+      .then(() => {
+        window.location = '/after_login';
+      });
+  });
+});
+
