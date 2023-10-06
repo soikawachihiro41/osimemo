e512a8c83e0769d7902287d174ad6cdd0fa242ce
soikawa <soikawa0108@gmail.com>
2023-10-06 17:25:55 +0900

    管理者画面一旦削除

--- db/migrate/20231006060611_drop_active_admin_tables.rb
+++
@@ -1,6 +0,0 @@
-class DropActiveAdminTables < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :active_admin_comments
-    drop_table :admin_users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_060741) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_063108) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_060741) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
