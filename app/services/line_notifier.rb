6d158bf1f290919e04a987a4f19107427e72aea9
soikawa <soikawa0108@gmail.com>
2023-10-24 20:32:00 +0900

    推しお誕生日通知

---
+++ app/services/line_notifier.rb
@@ -0,0 +1,17 @@
+# app/services/line_notifier.rb
+class LineNotifier
+  def self.send_birthday_notification(line_id, idol_name)
+    message = {
+      type: 'text',
+      text: "Happy Birthday to #{idol_name}! 🎉 ステキな1日を過ごしてね"
+    }
+
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+
+    response = client.push_message(line_id, message)
+    # 必要に応じて、レスポンスをチェックしてエラーを処理します
+  end
+end
---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,9 @@
+# lib/tasks/scheduler.rake
+desc "Send birthday notifications"
+task send_birthday_notifications: :environment do
+  idols = Idol.today_birthday.includes(:user)
+  idols.each do |idol|
+    # ここでLINE Messaging APIを使用して通知を送信します。
+    LineNotifier.send_birthday_notification(idol.user.line_id, idol.name)
+  end
+end
